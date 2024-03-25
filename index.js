9e39ab3db64850a2d7fca43202cdfd3639189f11
zaheem hassan <164178436+zaheem55@users.noreply.github.com>
2024-03-25 16:36:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ramdomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",
+    }
+]);
+if (answer.userGuessednumber === ramdomnumber) {
+    console.log("congtratulations you guessed right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
